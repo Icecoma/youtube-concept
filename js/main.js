30289ab7741651229d7d2dca3d74506067920d11
Icecoma <56739222+Icecoma@users.noreply.github.com>
2021-04-12 23:09:14 +0300

    Доработка слайдеров и контента на странице, начата работа над мобильной версией

---
+++ js/main.js
@@ -0,0 +1,81 @@
+const swiper = new Swiper('.sub_slider', {
+    loop: true,
+    slidesPerView: 6,
+    spaceBetween: 20,
+    breakpoints:{
+      1900:{
+        slidesPerView: 6,
+      },
+      1600:{
+        slidesPerView: 5,
+      },
+      1300:{
+        slidesPerView: 4,
+      },
+      992:{
+        slidesPerView: 3,
+      },
+      860:{
+        slidesPerView: 2,
+      },
+      320:{
+        slidesPerView: 1,
+      }
+    },
+    navigation: {
+      nextEl: '.swiper_button_next',
+      prevEl: '.swiper_button_prev',
+    },
+  
+  });
+  const swiper2 = new Swiper('.rec_slider', {
+    loop: true,
+    slidesPerView: 3,
+    spaceBetween: 20,
+    breakpoints:{
+      1020:{
+        slidesPerView: 3,
+      },
+      860:{
+        slidesPerView: 2,
+      },
+      320:{
+        slidesPerView: 1,
+      }
+    },
+    navigation: {
+      nextEl: '.recswiper_button_next',
+      prevEl: '.recswiper_button_prev',
+    },
+  
+  });
+  const swiper3 = new Swiper('.fooddrink_slider', {
+    loop: true,
+    slidesPerView: 6,
+    spaceBetween: 20,
+    breakpoints:{
+      1900:{
+        slidesPerView: 6,
+      },
+      1600:{
+        slidesPerView: 5,
+      },
+      1300:{
+        slidesPerView: 4,
+      },
+      992:{
+        slidesPerView: 3,
+      },
+      860:{
+        slidesPerView: 2,
+      },
+      320:{
+        slidesPerView: 1,
+      }
+    },
+    navigation: {
+      nextEl: '.fdswiper_button_next',
+      prevEl: '.fdswiper_button_prev',
+    },
+  
+  });
